5674e906da48fa21bcf1d66508d20ae72aa9590f
Spadari Sara <saraspadari@yahoo.com>
2026-02-06 12:58:17 +0100

    backback

--- App.tsx
+++ App.tsx
@@ -37,19 +37,19 @@ const App: React.FC = () => {
       <main className="max-w-[1440px] mx-auto px-6 md:px-12 lg:px-24 transition-opacity duration-1000 opacity-0 pb-24">
         <Header />
 
-        <section id="experience" className="mt-24 md:mt-24">
+        <section id="experience" className="mt-24">
           <Experience />
         </section>
 
-        <section id="about" className="mt-28 md:mt-28">
+        <section id="about" className="mt-28">
           <About />
         </section>
 
-        <section id="education-skills" className="mt-24 md:mt-24>
+        <section id="education-skills" className="mt-24">
           <EducationSkills />
         </section>
 
-        <section id="contact" className="mt-10 border-t border-white/10 pt-24  md:pt-20">
+        <section id="contact" className="mt-10 border-t border-white/10 pt-24">
           <Footer />
         </section>
       </main>
